2e05780bd52d8c2d9bc74aa25b3572795ed00b01
sthai1721 <sophia.thai@rutgers.edu>
2018-08-10 14:25:32 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,7 +1,18 @@
 # Code your instances here
 class GardenGnome
   
+  def initialize(name)
+    @name = name
+  end
   
+  def name(name)
+    @name
+  end
+  
+  def name=(new_name)
+    @name= new_name
+  end
+
 
 end
 
